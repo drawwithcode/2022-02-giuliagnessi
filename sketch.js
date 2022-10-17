795c30de830369528f9a501725e9fb64fd8b42b3
giuliagnessi <113455252+giuliagnessi@users.noreply.github.com>
2022-10-17 11:51:00 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,79 @@
-function preload() {
-	// put preload code here
-}
+// sfondo
+let b1;
+let b2;
+
+//myPalette
+const myPalette = ["#993955", "#AE76A6", "#A3C3D9", "#CCD6EB", "#E9ECF5"];
+const myPalette2 = ["#ED6A5A", "#F4F1BB", "#9BC1BC", "#5CA4A9", "#E6EBE0"];
+
+//costanti texture
+const a = 40;
+const b = 40;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  createCanvas(windowWidth, windowHeight);
+  strokeWeight(27);
+  frameRate(1.6);
+
+  // sfondo
+  b1 = color("#C8D5B9");
+  b2 = color("#68B0AB");
 }
 
 function draw() {
-	// put drawing code here
+  //bg
+
+  background(color(random(myPalette)));
+
+  if (mouseIsPressed) {
+    background(color(random(myPalette2)));
+  }
+
+  for (let x = a; x <= width; x += a * 1.75) {
+    for (let y = a; y <= height; y += a * 1.75) {
+      if (mouseX < windowWidth / 2) {
+        stroke(color(random(myPalette)));
+        noFill();
+      } else {
+        fill(color(random(myPalette)));
+        noStroke();
+      }
+
+      if (mouseIsPressed) {
+        fill(color(random(myPalette2)));
+        stroke(color(random(myPalette2)));
+      }
+
+      ellipse(x, y, a);
+    }
+  }
+
+  for (let x = b; x <= width; x += b * 2) {
+    for (let y = b; y <= height; y += b * 2) {
+      if (mouseX < windowHeight / 2) {
+        fill(color(random(myPalette)));
+        noStroke();
+      } else {
+        stroke(color(random(myPalette)));
+        noFill();
+      }
+
+      if (mouseIsPressed) {
+        fill(color(random(myPalette2)));
+        stroke(color(random(myPalette2)));
+      }
+    }
+  }
+
+  //text
+  noStroke();
+  textFont("Roboto Mono");
+  fill("BLACK");
+  textSize(23);
+  text("slide or click to change the wallpaper", mouseX, mouseY);
 }
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
